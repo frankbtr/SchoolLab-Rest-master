ae55efb7c4d1813be932559800b905e79f17d605
Frank Batur <frank_batur@outlook.com>
2023-06-18 17:27:54 -0400

    added WeatherApiClient

---
+++ src/main/java/com/cydeo/client/WeatherApiClient.java
@@ -0,0 +1,20 @@
+package com.cydeo.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(url = "http://api.weatherstack.com", name = "WEATHER-CLIENT")
+public interface WeatherApiClient {
+    // http://api.weatherstack.com  --> URL
+    // current --> endpoint
+    // ?access_key=b8ed0f7ba1467aade8028d795b912185&
+    // &
+    // query=Helsinki
+
+    @GetMapping("/current")
+    Object getCurrentWeather(@RequestParam(value = "access_key") String accessKey,
+                             @RequestParam(value = "query") String city);
+
+
+}
